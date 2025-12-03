4c957c2a091b2e8eea52f0b4ca9fc04a2352afe9
souzlume <lucassmelo2020@gmail.com>
2025-12-03 18:45:26 -0300

    primeiro commit do projeto!

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
+</script>
+
+<template>
+  <Header />
+  <Hero />
+
+  <h2>InspiraPixel</h2>
+</template>
+
+<style scoped lang="scss"></style>
